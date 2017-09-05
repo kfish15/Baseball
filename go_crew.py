9bc93066664aea6d6490d20cf09e616a86c3b1e1
kfish15 <kodyfisher15@gmail.com>
2017-09-04 21:19:40 -0500

    need to figure out how to get around wild card issue

--- go_crew.py
+++ go_crew.py
@@ -13,35 +13,6 @@ import json
 #function to determine the status of a game, if no team selected
 #based on the progress, different data is returned
 
-def game_info():
-    if game['status']['status'] == "In Progress":
-        return '%s (%s) vs %s (%s) @ %s %s' % (
-                game['away_team_name'],
-                game['linescore']['r']['away'],
-                game['home_team_name'],
-                game['linescore']['r']['home'],
-                game['venue'],
-                game['status']['status']
-            )
-    elif (game['status']['status'] == "Final" or game['status']['status'] == "Game Over"):
-        return '%s (%s) vs %s (%s) @ %s %s' % (
-                game['away_team_name'],
-                game['linescore']['r']['away'],
-                game['home_team_name'],
-                game['linescore']['r']['home'],
-                game['venue'],
-                game['status']['status']
-            )
-    elif (game['status']['status'] == "Pre-Game" or game['status']['status'] == "Preview"):
-        return '%s vs %s @ %s %s%s %s' % (
-                game['away_team_name'],
-                game['home_team_name'],
-                game['venue'],
-                game['home_time'],
-                game['hm_lg_ampm'],
-                game['status']['status'])
-
-
 
 def team_score():
     if game['status']['status'] == "In Progress":
@@ -49,18 +20,20 @@ def team_score():
         '-------------------------------\n' \
         '%s (%s) vs. %s (%s) @ %s\n' \
         '%s: %s of the %s\n' \
-        'Pitching: %s || Batting: %s || S: %s B: %s O: %s\n' \
+        'Pitching: %s, %s || Batting: %s, %s || S: %s B: %s O: %s\n' \
         '-------------------------------' % (
-                game['away_team_name'],
-                game['linescore']['r']['away'],
-                game['home_team_name'],
-                game['linescore']['r']['home'],
-                game['venue'],
-                game['status']['status'],
-                game['status']['inning_state'],
-                game['status']['inning'],
-                game['pitcher']['last'],
-                game['batter']['last'],
+				game['away_team_name'],
+				game['linescore']['r']['away'],
+				game['home_team_name'],
+				game['linescore']['r']['home'],
+				game['venue'],
+				game['status']['status'],
+				game['status']['inning_state'],
+				game['status']['inning'],
+				game['pitcher']['last'],
+				game['pitcher']['first'][:1],
+				game['batter']['last'],
+				game['batter']['first'][:1],
                 game['status']['s'],
                 game['status']['b'],
                 game['status']['o']
@@ -68,18 +41,20 @@ def team_score():
     elif (game['status']['status'] == "Final" or game['status']['status'] == "Game Over"):
         return \
         '-------------------------------\n' \
-        '%s (%s) vs. %s (%s) @ %s %s\n' \
-        'W: %s || L: %s || SV: %s\n' \
+        '%s (%s) vs. %s (%s) %s\n' \
+        'W: %s, %s || L: %s, %s || SV: %s %s\n' \
         '-------------------------------' % (
-                game['away_team_name'],
-                game['linescore']['r']['away'],
-                game['home_team_name'],
-                game['linescore']['r']['home'],
-                game['venue'],
+				game['away_team_name'],
+				game['linescore']['r']['away'],
+				game['home_team_name'],
+				game['linescore']['r']['home'],
 				game['status']['status'].upper(),
-                game['winning_pitcher']['name_display_roster'],
-                game['losing_pitcher']['name_display_roster'],
-                game['save_pitcher']['name_display_roster']
+				game['winning_pitcher']['last'],
+				game['winning_pitcher']['first'][:1],
+				game['losing_pitcher']['last'],
+				game['losing_pitcher']['first'][:1],
+				game['save_pitcher']['last'],
+				game['save_pitcher']['first'][:1]
             )
     elif (game['status']['status'] == "Pre-Game" or game['status']['status'] == "Preview"):
         return \
@@ -122,31 +97,15 @@ date = "today"
 baseball_data = requests.get(date_url(date))
 game_data = baseball_data.json()
 game_array = game_data['data']['games']['game']
-"""
 #team = raw_input('What team do you want? ex. SF or NYY ').upper()
 
-for game in game_array:
-    if team == "":
-        disp = game_info()
-        if disp:
-            print disp
-    else:
-        if (game['home_name_abbrev']) == team or (game['away_name_abbrev']) == team:
-            disp = team_score()
-            if disp:
-                print disp
-"""
 teams = ["MIL","COL","STL","CHC","ARI"]
 
 for game in game_array:
 	if game['home_name_abbrev'] not in teams or game['away_name_abbrev'] not in teams:
-		if game['status']['status'] == "In Progress":
-			disp = team_score()	
-			print disp
-		elif game['status']['status'] == "Final" or game['status']['status'] == "Game Over":
-			disp = team_score()
-			print disp
-		elif (game['status']['status'] == "Pre-Game" or game['status']['status'] == "Preview"):
-			disp = team_score()
-			print disp
-	#print disp 
+		disp = team_score()
+		print disp
+
+
+
+
